813684adb83fd45bade1a1e38807d3e709dfb63d
Shriphani Palakodety <shriphanip@gmail.com>
2014-09-04 15:00:25 -0400

    added files

--- src/topix_crawler/core.clj
+++ src/topix_crawler/core.clj
@@ -1,11 +1,15 @@
 (ns topix-crawler.core
-  (:require [clojure.tools.cli :refer [parse-opts]]
+  (:require [clojure.string :as string]
+            [clojure.tools.cli :refer [parse-opts]]
             [topix-crawler.list :as list]
-            [topix-crawler.extract-topic-names :as extract]))
+            [topix-crawler.extract-topic-names :as extract]
+            [topix-crawler.index-page-crawler :as index-crawler]))
 
 (def cmdline-options
   [["-c" "--crawl" "Crawl bro"]
-   ["-e" "--extract C" "Extract topix forums from corpus-file"]])
+   ["-e" "--extract C" "Extract topix forums from corpus-file"]
+   ["-i" "--index F" "Step through index pages"]
+   [nil "--write-to F" "File to write to"]])
 
 (def topix-all-forums-uri "http://www.topix.com/forum/dir")
 
@@ -17,4 +21,11 @@
           (list/crawl topix-all-forums-uri)
 
           (:extract options)
-          (extract/process-dir-corpus (:extract options)))))
+          (extract/process-dir-corpus (:extract options))
+
+          (:index options)
+          (let [index-pages (string/split-lines
+                             (slurp (:index options)))]
+            (doseq [uri index-pages]
+              (index-crawler/crawl uri
+                                   (:write-to options)))))))
---
+++ src/topix_crawler/index_page_crawler.clj
@@ -0,0 +1,61 @@
+(ns topix-crawler.index-page-crawler
+  "Topix index page crawler"
+  (:require [clj-http.client :as client]
+            [clojure.java.io :as io]
+            [net.cgrand.enlive-html :as html]
+            (org.bovinegenius [exploding-fish :as uri]))
+  (:import [java.io StringReader])
+  (:use [clojure.pprint :only [pprint]]))
+
+(defn write-bodies
+  [writer uri body]
+  (pprint {:uri  uri
+           :body body}
+          writer))
+
+(defn crawl
+  ([index-page-uri filename]
+     (let [writer (io/writer filename :append true)]
+       (crawl index-page-uri
+              (set [])
+              writer)))
+
+  ([uri-to-get crawled writer]
+     (println :downloading uri-to-get)
+     (if uri-to-get
+       (let [body (:body
+                   (client/get
+                    uri-to-get
+                    {:headers
+                     {"User-Agent"
+                      (str "Mozilla/5.0 (Windows NT 6.1;) Gecko/20100101 Firefox/13.0.1"
+                           " lemurproject crawler, version 1.0"
+                           " contact: http://boston.lti.cs.cmu.edu/clueweb12/")}}))
+             
+             next-pg-anchor (-> body
+                                (StringReader.)
+                                html/html-resource
+                                (html/select [:a.x-thread-pagination]))
+             
+             paginator (first
+                        (filter
+                         (fn [a]
+                           (re-find #"Next" (html/text a)))
+                         next-pg-anchor))
+             
+             next-uri-to-get
+             (uri/resolve-uri uri-to-get
+                              (-> paginator
+                                  :attrs
+                                  :href))]
+         (do (write-bodies writer
+                           uri-to-get
+                           body)
+             (Thread/sleep
+              (+ 1500
+                 (rand-int 7000)))
+             (recur next-uri-to-get
+                    (cons uri-to-get crawled)
+                    writer)))
+       (do (println :done)
+           (.close writer)))))
